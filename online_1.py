f2b75ad85e260de2da697f99b5dfa429ad85ee40
kenes111 <petrech@gmail.com>
2025-09-13 13:31:32 +0200

    online skolenie 1

---
+++ online_1.py
@@ -0,0 +1,38 @@
+# s = " 1,2,3,4,5,6,7,8,9,10"
+# first_number = s.startswith(" 1")
+# print(first_number)
+# s = s.strip()
+# print(s)
+# s = s[0:3]
+# print(s)
+# from curses.ascii import isupper
+# from http.cookiejar import uppercase_escaped_char
+
+# vstup  = "C++ je [kolkokrat] KRÁT lepší! "
+# vstup_bez_medzier = vstup.strip()
+# print(vstup_bez_medzier)
+# vstup_malymi = vstup.lower()
+# print(vstup_malymi)
+# nahrada = vstup.replace("C++","Python")
+# print(nahrada)
+# print(nahrada.startswith("Python"))
+# pocet = len(vstup)
+# vstup = nahrada.replace ("[kolkokrat]", str((pocet * 3)))
+# print(vstup)
+
+
+# a = int(input("zadaj prve cislo"))
+# b = int(input("zadaj druhe cislo"))
+# if a == b:
+#     print("su rovne")
+# elif a > b:
+#     print("prve je vacsie")
+# else:
+#     print("druhe je vacsie")
+
+a = str(input("zadaj retzec"))
+if a[0].isupper():
+    print("je velkym")
+
+
+
